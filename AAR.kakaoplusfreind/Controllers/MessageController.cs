908208f77002716b2476e765ca78e7d3e4983c93
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2018-05-14 21:56:17 +0900

    clean up

--- AAR.kakaoplusfreind/Controllers/MessageController.cs
+++ AAR.kakaoplusfreind/Controllers/MessageController.cs
@@ -28,8 +28,6 @@ namespace AAR.kakaoplusfreind.Controllers
 
         private ConversationInfo conversationinfo;
         private My_DBService myDbService;
-        string mytest = "heni";
-        bool myval = false;
 
         // GET: Message 
         public async Task<ActionResult> Index(string user_key, string type, string content)
@@ -51,7 +49,7 @@ namespace AAR.kakaoplusfreind.Controllers
 
             if (results.Count() != 0)
             {
-                Conversation = Client.Conversations.ReconnectToConversation(item.coversation.ConversationId.ToString()); myval = true;
+                Conversation = Client.Conversations.ReconnectToConversation(item.coversation.ConversationId.ToString());
             }
             else
             {
@@ -77,108 +75,23 @@ namespace AAR.kakaoplusfreind.Controllers
             };
 
             await Client.Conversations.PostActivityAsync(this.Conversation.ConversationId, userMessage);
-
-
-
-
+            
             // the part that receives the message 
             string watermark = null;
 
             while (true)
             {
-                //var activitySet = await Client.Conversations.GetActivitiesAsync(Conversation.ConversationId, watermark);
-                //watermark = activitySet?.Watermark;
-
-                //var activities = from x in activitySet.Activities where x.From.Id == botId select x;
-
-
-                //conversationinfo = new ConversationInfo
-                //{
-                //    coversation = Conversation,
-                //    id = user_key,
-                //    timestamp = DateTimeOffset.Now,
-                //    watermark = watermark
-                //};
-
-                //if (results.Count() == 0)
-                //    await myDbService.SetInfoAsync(conversationinfo);
-
-
-                //Message message = new Message();
-                //MessageResponse messageResponse = new MessageResponse();
-                //messageResponse.message = message;
-
-                //foreach (Activity activity in activities)
-                //{
-                //    message.text = ((Activity)activities.Last()).Text +  activities.Count()+activity.Text + "-" + this.Conversation.ConversationId + " # " + user_key + "#" + Session["cid"] + "###"+mytest;
-                //}
-
-                //return Json(messageResponse, JsonRequestBehavior.AllowGet);             // return View (); 
-
-                //=============================================================================================================
+                
                 var activitySet = await Client.Conversations.GetActivitiesAsync(Conversation.ConversationId, watermark);
                 watermark = activitySet?.Watermark;
                 var response = from x in activitySet.Activities where x.From.Id == botId select x;
 
-                // 발견된 복수의 Activity를 넘겨서 처리
+                // Handle multiple discovered activities - 발견된 복수의 Activity를 넘겨서 처리
                 var msg = MessageConvertor.DirectLineToKakao(response.ToList());
                 return Json(msg);
             }
         }
 
-        //============================================================================================================
-
-
-        //private IDirectLineConversationService conversationService;
-
-        //public MessageController(IDirectLineConversationService service)
-        //{
-        //    conversationService = service;
-        //}
-
-        ///// <summary>
-        ///// 실제 카카오톡과 메시지를 주고 받는 부분. 
-        ///// 
-        ///// </summary>
-        ///// <param name="user_key">해시된 사용자 키</param>
-        ///// <param name="type">text / photo </param>
-        ///// <param name="content">내용</param>
-        ///// <returns></returns>
-        //[AcceptVerbs(HttpVerbs.Post)]
-        //public async Task<ActionResult> Index(string user_key, string type, string content)
-        //{
-        //    try
-        //    {
-        //        // covert from Kakao talk message to Bot Builder Activity
-        //        Activity activity = new Activity
-        //        {
-        //            // Bot 에서 메시지가 kakao로 부터 요청되었음을 알수 있도록 name에 kakao를 써준다. 
-        //            From = new ChannelAccount(id: user_key, name: "kakao"),
-        //            Type = ActivityTypes.Message
-        //        };
-        //        if (type == "text")
-        //        {
-        //            activity.Text = content;
-        //        }
-        //        else if (type == "photo")
-        //        {
-        //            activity.Attachments = new List<Attachment>();
-        //            activity.Attachments.Add(new Attachment
-        //            {
-        //                ContentUrl = content
-        //            });
-        //        }
-        //        var response = await conversationService.SendAndReceiveMessageAsync(user_key, activity);
-        //        // 발견된 복수의 Activity를 넘겨서 처리
-        //        var msg = MessageConvertor.DirectLineToKakao(response);
-        //        return Json(msg);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new InvalidOperationException("Direct Line 연결오류", ex);
-        //    }
-        //}
-
-        //=================================================================================
+        
     }
 }
